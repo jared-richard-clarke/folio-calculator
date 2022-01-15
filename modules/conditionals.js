c28beae352f30c3ce07189413793827423a9d49a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-15 13:09:39 -0800

    Add examples to conditionals module

--- modules/conditionals.js
+++ modules/conditionals.js
@@ -1,10 +1,12 @@
-// conditionals.js
+// const conditionals = readonly {
 //   1. and(...expressions) -> boolean
 //      If any expression evaluates false, stop evaluation and return false.
+//      and(1 < 2, 3 === 3) -> true
 //
 //   2. or(...expressions) -> boolean
 //      If any expression evaluates true, stop evaluation and return true.
-// 
+//      or(1 < 2, 3 !== 3) -> true
+// }
 // purpose: Module "conditionals" provides functional replacements for && and || operators.
 
 const edge_cases = new Map([
@@ -18,11 +20,13 @@ const edge_cases = new Map([
     [undefined, false],
     [null, false],
 ]);
+// 1. function and
 function and(...expressions) {
     return expressions.every((value) => {
         return edge_cases.has(value) ? edge_cases.get(value) : value;
     });
 }
+// 2. function or
 function or(...expressions) {
     return expressions.some((value) => {
         return edge_cases.has(value) ? edge_cases.get(value) : value;
