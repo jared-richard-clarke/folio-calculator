ba4f11ea7ca387c3103afb728feb7ba65f414a4a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-01-19 02:10:16 -0800

    Replace symbols.js with constants.js

---
+++ modules/constants.js
@@ -0,0 +1,19 @@
+// Consolidates variables.
+const ADD = "+";
+const SUB = "-";
+// ----------------
+const MUL = "×";     // symbolic multiplier.
+const IMP = "imp-×"; // implied multiplier
+const LML = "*";     // literal multiplier recognized by JavaScript.
+// ----------------
+const DIV = "÷";     // symbolic divider.
+const LDV = "/";     // literal divider recognized by JavaScript.
+// ----------------
+const EXP = "^";
+// ----------------
+const OPE = "(";
+const CLO = ")";
+// ----------------
+const OPERATORS = SUB + ADD + MUL + DIV + EXP; // "-+×÷^"
+
+export { ADD, SUB, MUL, IMP, LML, DIV, LDV, EXP, OPE, CLO, OPERATORS };
