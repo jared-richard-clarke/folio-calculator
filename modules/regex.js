85e6accf59d13979d0c4767182ac0c9de27cd76f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-03-15 16:35:28 -0700

    Fix divide-by-zero regex

--- modules/regex.js
+++ modules/regex.js
@@ -62,7 +62,8 @@ const TRAILING_ZERO = new RegExp("[" + constants.OPERATORS + "]\\s0$"); // /[-+�
 const DECIMAL = /\d+\.\d+$/;
 const TRAILING_DECIMAL = /\d\.$/;
 const TRAILING_DIGIT = /\d$/;
-const DIVIDE_BY_ZERO = new RegExp("\\s" + DIV + "\\s0\\s|\\s" + DIV + "\\s0$"); // \s÷\s0\s|\s÷\s0$;
+// Use literal regular expression. "new Regular expression" syntax overly complicated.
+const DIVIDE_BY_ZERO = /\s÷\s0\.?\s|\s÷\s0\.?$/;
 
 function check_text(regex) {
     return function (text) {
