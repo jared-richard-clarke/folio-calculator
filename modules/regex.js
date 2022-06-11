866a02596c12f6181a70bb972b16cdbfd1979e28
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-06-10 20:46:04 -0700

    Add is_trailing_e

--- modules/regex.js
+++ modules/regex.js
@@ -45,7 +45,11 @@ import constants from "./constants.js";
 //      Is expression OVERFLOW_ERROR constant?
 //      is_overflow_error(OVERFLOW_ERROR) -> true
 //
-//  12. is_single_char(string) -> boolean
+//  12. is_trailing_e(string) -> boolean
+//      Does expression end with "e", debris from exponent notation?
+//      is_trailing_e("4e") -> true
+//
+//  13. is_single_char(string) -> boolean
 //      Is expression one character?
 //      is_single_char("1") -> true
 // }
@@ -64,6 +68,7 @@ const TRAILING_DECIMAL = /\d\.$/;
 const TRAILING_DIGIT = /\d$/;
 // Use literal regular expression. "new Regular expression" syntax overly complicated.
 const DIVIDE_BY_ZERO = /\s÷\s0\.?\s|\s÷\s0\.?$/;
+const TRAILING_E = /e$/;
 
 function check_text(regex) {
     return function (text) {
@@ -96,6 +101,8 @@ export default Object.freeze({
     is_zero_error: check_text(ZERO_ERROR),
     // 11. function is_overflow_error
     is_overflow_error: check_text(OVERFLOW_ERROR),
-    // 12. function is_single_char
+    // 12. function is_trailing_e
+    is_trailing_e: check_text(TRAILING_E),
+    // 13. function is_single_char
     is_single_char,
 });
